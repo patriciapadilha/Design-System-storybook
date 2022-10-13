53df995b97195aa8788ff4a98174913f0f07112d
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-10-13 20:02:50 -0300

    ci: change config.base in main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-storybook/'
+      config.base = '/Design-System-storybook/'
     }
     return config;
   }
